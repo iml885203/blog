4d679f92c98480b7a526a70679e8f170212f93cc
Logan <iml885203@gmail.com>
2022-12-02 14:51:47 +0800

    Site updated: 2022-12-02 14:51:46

--- js/third-party/statistics/firestore.js
+++ js/third-party/statistics/firestore.js
@@ -5,9 +5,9 @@ let r=o.exists?o.data().count:0;
 // If first view this article
 return t&&(
 // Increase count
-r++,e.set({count:r})),r})),t=e=>t=>{e.innerText=t},o=firebase.firestore().collection(CONFIG.firestore.collection);document.addEventListener("page:loaded",(()=>{if(CONFIG.page.isPost){
+r++,e.set({count:r})),r})),t=firebase.firestore().collection(CONFIG.firestore.collection);document.addEventListener("page:loaded",(()=>{if(CONFIG.page.isPost){
 // Fix issue #118
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent
-const r=document.querySelector(".post-title").textContent.trim(),n=o.doc(r);let i=CONFIG.hostname===location.hostname;localStorage.getItem(r)?i=!1:
+const o=document.querySelector(".post-title").textContent.trim(),r=t.doc(o);let n=CONFIG.hostname===location.hostname;localStorage.getItem(o)?n=!1:
 // Mark as visited
-localStorage.setItem(r,!0),e(n,i).then(t(document.querySelector(".firestore-visitors-count")))}else if(CONFIG.page.isHome){const r=[...document.querySelectorAll(".post-title")].map((t=>{const r=t.textContent.trim(),n=o.doc(r);return e(n)}));Promise.all(r).then((e=>{const o=document.querySelectorAll(".firestore-visitors-count");e.forEach(((e,r)=>{t(o[r])(e)}))}))}}))}();
+localStorage.setItem(o,!0),e(r,n).then((e=>{document.querySelector(".firestore-visitors-count").innerText=e}))}else if(CONFIG.page.isHome){const o=[...document.querySelectorAll(".post-title")].map((o=>{const r=o.textContent.trim(),n=t.doc(r);return e(n)}));Promise.all(o).then((e=>{const t=document.querySelectorAll(".firestore-visitors-count");e.forEach(((e,o)=>{t[o].innerText=e}))}))}}))}();
